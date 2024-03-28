b5d96bba1581faabf3e0f95558dab8f28a6fa2fc
elena_e <elenaka123@gmail.com>
2024-03-28 11:07:52 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var shoutIt = function (name) {
+    var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
+
+    return shout;
+};
+
+console.log(shoutIt("Elena"));
+console.log(shoutIt("Hunter"));
+console.log(shoutIt("Onyx"));
+console.log(shoutIt("Xavier"));
